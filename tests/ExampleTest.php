38f8c3910d38531024d25a6fe763291deed40e85
Cosnavel <coding@niclaskahlmeier.de>
2021-08-06 23:51:38 +0200

    started with tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,11 +2,57 @@
 
 namespace Cosnavel\LaravelQueryLocalization\Tests;
 
-class ExampleTest extends TestCase
+use Cosnavel\LaravelQueryLocalization\LaravelQueryLocalization;
+
+class LaravelQueryLocalizationTest extends TestCase
 {
+
     /** @test */
-    public function true_is_true()
+    public function it_can_resolve_the_localization_manager()
     {
-        $this->assertTrue(true);
+        $this->assertTrue($this->localization instanceof LaravelQueryLocalization);
+    }
+
+    /** @test */
+    public function it_can_get_the_current_locale()
+    {
+        $this->assertEquals($this->localization->getCurrentLocale(), 'en');
+    }
+
+    /** @test */
+    public function it_can_a_valid_locale()
+    {
+        $this->assertEquals($this->localization->getCurrentLocale(), 'en');
+        $this->localization->setLocale('de');
+        $this->assertEquals($this->localization->getCurrentLocale(), 'de');
+    }
+
+    /** @test */
+    public function it_set_default_locale_if_a_invalid_locale_tried_to_set()
+    {
+        $this->assertEquals($this->localization->getCurrentLocale(), 'en');
+        $this->localization->setLocale('ru');
+        $this->assertEquals($this->localization->getCurrentLocale(), 'en');
+    }
+
+
+    /** @test */
+    public function it_can_get_the_supported_locales()
+    {
+        $this->assertEquals($this->locales, $this->localization->getSupportedLocales());
+    }
+
+    /** @test */
+    public function it_can_get_the_supported_locales_keys()
+    {
+        $this->assertEquals(array_keys($this->locales), $this->localization->getSupportedLanguagesKeys());
+    }
+
+    /** @test */
+    public function it_can_check_if_a_locale_is_supported()
+    {
+        $this->assertEquals($this->localization->determineValidLanguage('en'), 'en');
+        $this->assertEquals($this->localization->determineValidLanguage('de'), 'de');
+        $this->assertEquals($this->localization->determineValidLanguage('deruen'), 'en');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,12 +8,21 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    protected $localization;
+
+    protected $locales = [
+        'de'          => ['name' => 'Deutsch',                 'script' => 'Latn', 'native' => 'Deutsch', 'regional' => 'de_DE', 'flag' => '🇩🇪'],
+        'en'          => ['name' => 'English',                'script' => 'Latn', 'native' => 'English', 'regional' => 'en_US', 'flag' => '🇺🇸'],
+    ];
+
+
+
     public function setUp(): void
     {
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Cosnavel\\LaravelQueryLocalization\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Cosnavel\\LaravelQueryLocalization\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
@@ -27,6 +36,18 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
+
+
+        $this->localization = $app['laravel-query-localization'];
+
+        $app->setLocale('en');
+
+        $app['config']->set([
+            'localization.supportedLocales' => $this->locales,
+            'query-localization.useAcceptLanguageHeader' => true,
+            'query-localization.useUserLanguagePreference' => true,
+        ]);
+
 
         /*
         include_once __DIR__.'/../database/migrations/create_laravel-query-localization_table.php.stub';
